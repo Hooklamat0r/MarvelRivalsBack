1f5e3ff1da401d615d0b0610b79403a3bc807ecf
Hook <73350763+Hooklamat0r@users.noreply.github.com>
2026-03-17 20:17:35 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,21 @@
 <?php
-
 use App\Kernel;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require dirname(__DIR__).'/vendor/autoload.php';
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+// No cargar Dotenv en producción
+$env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'prod';
+$debug = (bool) ($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? false);
+
+if ($debug) {
+    umask(0000);
+    Debug::enable();
+}
+
+$kernel = new Kernel($env, $debug);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
